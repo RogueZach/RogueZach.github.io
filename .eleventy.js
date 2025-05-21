8a6b26c5d928fdd477235dd706bc565ae233eb7d
Zachary Amith <zacharyamith@outlook.com>
2025-05-21 15:23:43 -0700

    going rogue again....
    hate the layout lol

--- .eleventy.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(eleventyConfig) {
-  return {
-    dir: {
-      input: "_posts",
-      includes: "../_includes",
-      output: "_site"
-    }
-  };
-};
--- _posts/hello-world.md
+++
@@ -1,7 +0,0 @@
----
-title: "Hello World"
-date: 2025-05-21
-layout: layout.njk
----
-
-Welcome to my **first blog post** with Eleventy! This blog is generated from Markdown files.
